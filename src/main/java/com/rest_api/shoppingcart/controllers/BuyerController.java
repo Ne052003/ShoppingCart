731c34bbc638e886cc815d6abc449b479b8c226e
Neoly Moreno <neolyamr@gmail.com>
2023-07-13 15:32:23 -0500

    Add methods to BuyerController and OrderController

--- src/main/java/com/rest_api/shoppingcart/controllers/BuyerController.java
+++ src/main/java/com/rest_api/shoppingcart/controllers/BuyerController.java
@@ -1,9 +1,22 @@
 package com.rest_api.shoppingcart.controllers;
 
+import com.rest_api.shoppingcart.entities.Buyer;
+import com.rest_api.shoppingcart.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/buyers")
 public class BuyerController {
+    @Autowired
+    private UserService userService;
+
+    public ResponseEntity<?> disableBuyer(Buyer buyer) {
+        if (userService.disableBuyer(buyer)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/rest_api/shoppingcart/controllers/OrderController.java
+++ src/main/java/com/rest_api/shoppingcart/controllers/OrderController.java
@@ -1,9 +1,60 @@
 package com.rest_api.shoppingcart.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rest_api.shoppingcart.entities.Order;
+import com.rest_api.shoppingcart.entities.User;
+import com.rest_api.shoppingcart.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(Order order) {
+        Optional<Order> optOrder = orderService.createOrder(order);
+        if (optOrder.isPresent()) {
+            return ResponseEntity.created(URI.create("/" + order.getOrderId())).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Order> updateOrder(Order order) {
+        Optional<Order> optOrder = orderService.updateOrder(order);
+        if (optOrder.isPresent()) {
+            return ResponseEntity.ok(order);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/cancel/{orderId}")
+    public ResponseEntity<?> cancelOrder(@PathVariable Long orderId) {
+        if (orderService.cancelOrder(orderId)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrder(@PathVariable Long orderId) {
+        Optional<Order> optOrder = orderService.getOrderById(orderId);
+        return optOrder.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders(User user) {
+        List<Order> orders = orderService.getOrdersByUser(user);
+        if (!orders.isEmpty()) {
+            return ResponseEntity.ok(orders);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 }
