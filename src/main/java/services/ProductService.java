32629cb5d6c4b6bac3f3b0a36ced707f464015b7
Andres Johan Florez Gonzalez <89665982+johanflorez98@users.noreply.github.com>
2023-07-12 23:34:25 -0500

    Implement ProductService with methods: -Create -Delete -Update -ListById -List -Enabled

--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,25 +1,81 @@
 package services;
 
+import com.rest_api.shoppingcart.entities.Category;
 import com.rest_api.shoppingcart.entities.Product;
-import org.springframework.stereotype.Service;
-
+import com.rest_api.shoppingcart.repositories.CategoryRepository;
+import com.rest_api.shoppingcart.repositories.ProductRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+  @Autowired ProductRepository productRepository;
+  @Autowired CategoryRepository categoryRepository;
 
-    public Product createProduct(Product product) {
+  public Product createProduct(Product product) {
+    if (isInvalidProduct(product)) {
+      return null;
     }
 
-    public Product updateProduct(Product product) {
+    Category category = getCategoryIfExists(product.getCategory().getCategoryId());
+    if (category == null) {
+      return null;
     }
 
-    public void deleteProduct(Long productId) {
+    product.setCategory(category);
+    return productRepository.save(product);
+  }
+
+  public Product updateProduct(Product product) {
+    Optional<Product> existingProduct = productRepository.findById(product.getProductId());
+
+    if (isInvalidProduct(product) || existingProduct.isEmpty()) {
+      return null;
     }
 
-    public Product getProductById(Long productId) {
+    Category category = getCategoryIfExists(product.getCategory().getCategoryId());
+    if (category == null) {
+      return null;
     }
 
-    public List<Product> getAllProducts() {
-    }
+    Product updatedProduct = existingProduct.get();
+    updatedProduct.setName(product.getName());
+    updatedProduct.setPrice(product.getPrice());
+    updatedProduct.setCategory(category);
+
+    return productRepository.save(updatedProduct);
+  }
+
+  public void deleteProduct(Long productId) {
+    productRepository.findById(productId).ifPresent(productRepository::delete);
+  }
+
+  public Product getProductById(Long productId) {
+    return productRepository.findById(productId).orElse(null);
+  }
+
+  public List<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  public boolean setProductEnabled(Long productId, boolean enabled) {
+    return productRepository.findById(productId)
+            .map(product -> {
+              product.setEnabled(enabled);
+              productRepository.save(product);
+              return true;
+            })
+            .orElse(false);
+  }
+
+
+  private boolean isInvalidProduct(Product product) {
+    return product.getName() == null || product.getPrice() <= 0d || product.getCategory() == null;
+  }
+
+  private Category getCategoryIfExists(Long categoryId) {
+    return categoryRepository.findById(categoryId).orElse(null);
+  }
 }
