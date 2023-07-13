8fc23b3673fde731af8f302c8de010a6fcfa6b5c
Neoly Moreno <neolyamr@gmail.com>
2023-07-12 23:35:56 -0500

    Complete OrderService and UserService methods

--- src/main/java/services/OrderService.java
+++ src/main/java/services/OrderService.java
@@ -1,24 +1,51 @@
 package services;
 
 import com.rest_api.shoppingcart.entities.Order;
+import com.rest_api.shoppingcart.entities.User;
+import com.rest_api.shoppingcart.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
-    public Order createOrder(Order order) {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Optional<Order> createOrder(Order order) {
+        try {
+            orderRepository.save(order);
+            return orderRepository.findById(order.getOrderId());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return Optional.empty();
     }
 
-    public Order updateOrder(Order order) {
+    public Optional<Order> updateOrder(Order order) {
+        Optional<Order> optOrder = orderRepository.findById(order.getOrderId());
+        if (optOrder.isPresent()) {
+            orderRepository.save(order);
+            return optOrder;
+        }
+        return Optional.empty();
     }
 
     public void cancelOrder(Long orderId) {
+        Optional<Order> optOrder = orderRepository.findById(orderId);
+        if (optOrder.isPresent() && !optOrder.get().getCancelled()) {
+            optOrder.get().setCancelled(true);
+            orderRepository.save(optOrder.get());
+        }
     }
 
-    public Order getOrderById(Long orderId) {
+    public Optional<Order> getOrderById(Long orderId) {
+        return orderRepository.findById(orderId);
     }
 
-    public List<Order> getOrdersByUser(Long buyerId) {
+    public List<Order> getOrdersByUser(User user) {
+        return orderRepository.findByUser(user);
     }
 }
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,12 +1,24 @@
 package services;
 
-import com.rest_api.shoppingcart.repositories.UserRepository;
+import com.rest_api.shoppingcart.entities.Buyer;
+import com.rest_api.shoppingcart.repositories.BuyerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.rest_api.shoppingcart.entities.User;
 
-import java.util.List;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    private BuyerRepository buyerRepository;
+
+    public boolean disableBuyer(Buyer buyer) {
+        Optional<Buyer> optBuyer = buyerRepository.findById(buyer.getUserId());
+        if (optBuyer.isPresent()) {
+            optBuyer.get().setDisabled(true);
+            return true;
+        }
+        return false;
+    }
 }
