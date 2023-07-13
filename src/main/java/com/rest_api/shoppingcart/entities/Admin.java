a0febe75bcd3d733e33451460b14010c25258fa6
Neoly Moreno <neolyamr@gmail.com>
2023-07-12 23:26:49 -0500

    Add disabled field to Buyer and @Data annotation

--- src/main/java/com/rest_api/shoppingcart/entities/Admin.java
+++ src/main/java/com/rest_api/shoppingcart/entities/Admin.java
@@ -1,9 +1,10 @@
 package com.rest_api.shoppingcart.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @DiscriminatorValue("ADMIN")
 public class Admin extends User {
-  @Column private String adminProperty;
 }
--- src/main/java/com/rest_api/shoppingcart/entities/Buyer.java
+++ src/main/java/com/rest_api/shoppingcart/entities/Buyer.java
@@ -1,14 +1,15 @@
 package com.rest_api.shoppingcart.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
-
+@Data
 @Entity
 @DiscriminatorValue("BUYER")
 public class Buyer extends User {
     @Column
-    private String buyerProperty;
+    private Boolean disabled;
 
     @OneToMany(mappedBy = "buyer")
     private List<Order> orders;
