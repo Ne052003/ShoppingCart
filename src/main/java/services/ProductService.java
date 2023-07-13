0a44be4fc276b4272f64fe52027fa4d469fec1f8
Andres Johan Florez Gonzalez <89665982+johanflorez98@users.noreply.github.com>
2023-07-12 22:18:01 -0500

    Add services User, Order and Product and some template methods

---
+++ src/main/java/services/ProductService.java
@@ -0,0 +1,25 @@
+package services;
+
+import com.rest_api.shoppingcart.entities.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public Product createProduct(Product product) {
+    }
+
+    public Product updateProduct(Product product) {
+    }
+
+    public void deleteProduct(Long productId) {
+    }
+
+    public Product getProductById(Long productId) {
+    }
+
+    public List<Product> getAllProducts() {
+    }
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,12 @@
+package services;
+
+import com.rest_api.shoppingcart.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.rest_api.shoppingcart.entities.User;
+
+import java.util.List;
+
+@Service
+public class UserService {
+}
