13414899261da77c2ae10435a4c0b11d28829103
Andres Johan Florez Gonzalez <89665982+johanflorez98@users.noreply.github.com>
2023-07-13 15:30:57 -0500

    Create Product and Reference controllers

--- src/main/java/com/rest_api/shoppingcart/controllers/ProductController.java
+++ src/main/java/com/rest_api/shoppingcart/controllers/ProductController.java
@@ -1,9 +1,60 @@
 package com.rest_api.shoppingcart.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rest_api.shoppingcart.entities.Product;
+import com.rest_api.shoppingcart.services.ProductService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+  @Autowired private ProductService productService;
+
+  @PostMapping
+  public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+    Optional<Product> createdProduct = productService.createProduct(product);
+    return createdProduct
+        .map(p -> ResponseEntity.status(HttpStatus.CREATED).body(p))
+        .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+  }
+
+  @PutMapping("/{productId}")
+  public ResponseEntity<Product> updateProduct(
+      @PathVariable Long productId, @RequestBody Product product) {
+    Optional<Product> updatedProduct = productService.updateProduct(product);
+    return updatedProduct.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+    boolean deleted = productService.deleteProduct(productId);
+    return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<Product> getProductById(@PathVariable Long productId) {
+    Optional<Product> product = productService.getProductById(productId);
+    return product.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Product>> getAllProducts() {
+    List<Product> products = productService.getAllProducts();
+    if (products.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    } else {
+      return ResponseEntity.ok(products);
+    }
+  }
+
+  @PatchMapping("/{productId}/enabled")
+  public ResponseEntity<Void> setProductEnabled(
+      @PathVariable Long productId, @RequestParam(name = "enabled") boolean enabled) {
+    boolean updated = productService.setProductEnabled(productId, enabled);
+    return updated ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
+  }
 }
--- src/main/java/com/rest_api/shoppingcart/controllers/ReferenceController.java
+++ src/main/java/com/rest_api/shoppingcart/controllers/ReferenceController.java
@@ -1,9 +1,25 @@
 package com.rest_api.shoppingcart.controllers;
 
+import com.rest_api.shoppingcart.entities.Reference;
+import com.rest_api.shoppingcart.services.ReferenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/references")
 public class ReferenceController {
+    @Autowired
+    private ReferenceService referenceService;
+
+    @PostMapping
+    public ResponseEntity<Void> createReference(@RequestBody Reference reference) {
+        boolean created = referenceService.createReference(reference);
+        return created ? ResponseEntity.status(HttpStatus.CREATED).build()
+                : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
