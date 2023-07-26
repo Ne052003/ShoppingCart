a7431f299db4c3baddcbec837274add93c9aa508
Neoly Moreno <neolyamr@gmail.com>
2023-07-26 14:01:10 -0500

    add findByUsername method in BuyerRepository

--- src/main/java/com/rest_api/shoppingcart/repositories/BuyerRepository.java
+++ src/main/java/com/rest_api/shoppingcart/repositories/BuyerRepository.java
@@ -3,5 +3,8 @@ package com.rest_api.shoppingcart.repositories;
 import com.rest_api.shoppingcart.entities.Buyer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {
+    public Optional<Buyer> findByUsername(String username);
 }
