6d5b40bccf03f433396c95a06d84298dea9fcb4a
Neoly Moreno <neolyamr@gmail.com>
2023-07-26 15:27:54 -0500

    add create, get and delete methods to BuyerController

--- src/main/java/com/rest_api/shoppingcart/controllers/BuyerController.java
+++ src/main/java/com/rest_api/shoppingcart/controllers/BuyerController.java
@@ -4,8 +4,11 @@ import com.rest_api.shoppingcart.entities.Buyer;
 import com.rest_api.shoppingcart.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/buyers")
@@ -13,9 +16,42 @@ public class BuyerController {
     @Autowired
     private UserService userService;
 
-    public ResponseEntity<?> disableBuyer(Buyer buyer) {
-        if (userService.disableBuyer(buyer)) {
+    @PostMapping
+    public ResponseEntity<Buyer> createBuyer(@RequestBody Buyer buyer) {
+        Optional<Buyer> optBuyer = userService.createBuyer(buyer);
+        return optBuyer.map(value -> ResponseEntity.created(URI.create("/" + value.getUsername())).body(value)).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping("/{buyerId}")
+    public ResponseEntity<?> disableBuyer(@PathVariable Long buyerId) {
+        if (userService.disableBuyer(buyerId)) {
             return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getUserByUsername(@RequestParam("username") String username) {
+        Optional<Buyer> optBuyer = userService.getUserByUsername(username);
+        if (optBuyer.isPresent()) {
+            return ResponseEntity.ok(optBuyer);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Buyer>> getAllBuyers() {
+        List<Buyer> buyers = userService.getAllBuyers();
+        if (!buyers.isEmpty()) {
+            return ResponseEntity.ok(buyers);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteBuyer(@RequestBody Buyer buyer) {
+        if (userService.deleteBuyer(buyer)) {
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
