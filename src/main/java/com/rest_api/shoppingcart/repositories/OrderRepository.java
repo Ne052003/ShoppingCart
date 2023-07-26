c5391a685fc66f18b7a252a3b06fad2b3b0abf60
Neoly Moreno <neolyamr@gmail.com>
2023-07-26 13:34:35 -0500

    fix OrderRepository: change parameters

--- src/main/java/com/rest_api/shoppingcart/repositories/OrderRepository.java
+++ src/main/java/com/rest_api/shoppingcart/repositories/OrderRepository.java
@@ -1,12 +1,12 @@
 package com.rest_api.shoppingcart.repositories;
 
+import com.rest_api.shoppingcart.entities.Buyer;
 import com.rest_api.shoppingcart.entities.Order;
-import com.rest_api.shoppingcart.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findByUser(User user);
+    List<Order> findByBuyer(Buyer buyer);
 }
