c31b610977ac3f29bbe98a37e4f27d80332853d3
Neoly Moreno <neolyamr@gmail.com>
2023-07-27 00:09:55 -0500

    delete findAll method from ProductRepository

--- src/main/java/com/rest_api/shoppingcart/repositories/ProductRepository.java
+++ src/main/java/com/rest_api/shoppingcart/repositories/ProductRepository.java
@@ -2,15 +2,13 @@ package com.rest_api.shoppingcart.repositories;
 
 import com.rest_api.shoppingcart.entities.Category;
 import com.rest_api.shoppingcart.entities.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-  List<Product> findByCategory(Category category);
+    List<Product> findByCategory(Category category);
 
-  List<Product> findByNameContaining(String name);
-
-  Page<Product> findAll(Pageable pageable);
+    List<Product> findByNameContaining(String name);
 }
